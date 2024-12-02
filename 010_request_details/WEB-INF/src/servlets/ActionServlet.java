d7f81f85708c3236aad900b859a36aca04e89fe6
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-12-02 16:41:38 +0530

    Servlet Request Methods

---
+++ 010_request_details/WEB-INF/src/servlets/ActionServlet.java
@@ -0,0 +1,45 @@
+package servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ActionServlet extends HttpServlet 
+{
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+    {
+        response.setContentType("text/html");
+
+        PrintWriter pw = response.getWriter();
+
+        pw.write("<h1>Local Address : " + request.getLocalAddr() + "</h1>");
+
+        pw.write("<h1>Remote Address : " + request.getRemoteAddr() + "</h1>");
+
+        pw.write("<h1>Remote Port : " + request.getRemotePort() + "</h1>");
+
+        pw.write("<h1>Local Port : " + request.getLocalPort() +  "</h1>");
+
+        pw.write("<h1>Server Port : " + request.getServerPort() + "</h1>");
+
+        pw.write("<h1>Protocol : " + request.getProtocol() + "</h1>");
+
+        pw.write("<h1>Scheme : " + request.getScheme() + "</h1>");
+
+        pw.write("<h1>Server Name : " + request.getServerName() + "</h1>");
+
+        pw.write("<h1>Secure : " + request.isSecure() + "</h1>");
+
+        pw.write("<h1>Local Name : " + request.getLocalName() + "</h1>");
+
+        pw.write("<h1>Content Length : " + request.getContentLength() + "</h1>");
+
+
+
+        pw.flush();
+        pw.close();
+    }
+}
