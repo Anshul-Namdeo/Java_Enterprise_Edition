ce0c17abef3abc9c87ef7ceb298eac379b86ce7b
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-12-05 00:32:00 +0530

    parameters name expose

---
+++ 016_param_names/WEB-INF/src/servlets/SaveServlet.java
@@ -0,0 +1,29 @@
+package servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import java.util.Enumeration;
+
+public class SaveServlet extends HttpServlet 
+{
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+    {
+        response.setContentType("text/html");
+
+        PrintWriter pw = response.getWriter();
+
+        Enumeration<String> names = request.getParameterNames();
+
+        while(names.hasMoreElements())
+            pw.write("<h1>Param Name: " + names.nextElement() + "</h1>");
+
+            pw.flush();
+            pw.close();
+    }
+}
