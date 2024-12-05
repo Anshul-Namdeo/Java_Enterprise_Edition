58a1fd4cd35af7d74a6e3576f485447d3c03ba69
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-12-06 00:04:31 +0530

    open file by servlet req.

---
+++ 018_file_download/WEB-INF/src/servlets/DownloadServlet.java
@@ -0,0 +1,38 @@
+package servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.ServletContext;
+import javax.servlet.annotation.WebServlet;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+@WebServlet("/download.do")
+public class DownloadServlet extends HttpServlet 
+{
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+    {
+        response.setContentType("application/pdf");
+
+        OutputStream os = response.getOutputStream();
+
+        ServletContext context = getServletContext();
+
+        InputStream is = context.getResourceAsStream("/python.pdf");
+
+        byte[] arr = new byte[256];
+
+        int count = 0;
+        while((count=is.read(arr)) != -1)
+        {
+            os.write(arr);
+        }
+
+        os.flush();
+        os.close();
+    }
+}
