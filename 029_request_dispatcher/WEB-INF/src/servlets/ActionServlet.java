57a080384d0c5d2e03f2e102c5294c642f5f3375
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-12-10 21:34:53 +0530

    Two servlet one Thread

---
+++ 029_request_dispatcher/WEB-INF/src/servlets/ActionServlet.java
@@ -0,0 +1,24 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+
+import javax.servlet.RequestDispatcher;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/act.do")
+public class ActionServlet extends HttpServlet 
+{
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+    {
+        System.out.println("Action Servlet --------------------------" + Thread.currentThread().getId());
+
+        RequestDispatcher view = request.getRequestDispatcher("pro.do");
+        view.forward(request, response);
+    }
+}
