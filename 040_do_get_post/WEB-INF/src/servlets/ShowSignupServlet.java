402c2b131f0d0dca5d963a97a0fbe3e56f4e7766
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-12-12 21:48:08 +0530

    doGet & doPost In one servlet

---
+++ 040_do_get_post/WEB-INF/src/servlets/ShowSignupServlet.java
@@ -0,0 +1,19 @@
+// package servlets;
+
+// import java.io.IOException;
+
+// import javax.servlet.http.HttpServlet;
+// import javax.servlet.http.HttpServletRequest;
+// import javax.servlet.http.HttpServletResponse;
+// import javax.servlet.ServletException;
+
+// import javax.servlet.annotation.WebServlet;
+
+// @WebServlet("/show_signup.do")
+// public class ShowSignupServlet extends HttpServlet 
+// {
+//     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+//     {
+//         request.getRequestDispatcher("signup.html").forward(request, response);
+//     }
+// }
