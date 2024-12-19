bd1ce55ae147e9c06217d6d912008ebdc64ba3d5
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-12-19 18:58:03 +0530

    one session obj. per client

---
+++ 042_session_getId/WEB-INF/src/servlets/CServlet.java
@@ -0,0 +1,29 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+
+import javax.servlet.annotation.WebServlet;
+
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/c.do")
+public class CServlet extends HttpServlet
+{
+    public void doGet(HttpServletRequest request , HttpServletResponse response)throws IOException, ServletException
+    {
+        HttpSession session = request.getSession();
+
+        String sessionId = session.getId();
+
+        System.out.println("CServlet----------SessionId: " + sessionId);
+
+        request.getRequestDispatcher("next.html").forward(request, response);
+
+
+    }
+}
