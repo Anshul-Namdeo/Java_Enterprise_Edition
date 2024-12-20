24e2529c8e67b4b3a9e77bd9ccede245a4bfde62
Anshul-Namdeo <anshulnamdeo280@gmail.com>
2024-12-20 23:02:53 +0530

    session creation & last accessed

---
+++ 046_session_timing/WEB-INF/src/servlets/BServlet.java
@@ -0,0 +1,39 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+
+import javax.servlet.annotation.WebServlet;
+
+import javax.servlet.http.HttpSession;
+
+import java.util.Date;
+
+@WebServlet("/b.do")
+public class BServlet extends HttpServlet
+{
+    public void doGet(HttpServletRequest request , HttpServletResponse response)throws IOException, ServletException
+    {
+        HttpSession session = request.getSession();
+
+        String sessionId = session.getId();
+
+        boolean sessionNayaHaiKya = session.isNew();
+
+        int maxInactiveInterval = session.getMaxInactiveInterval();
+
+        Date sessionCreationTime = new Date(session.getCreationTime());
+
+        Date sessionLastAccessedTime = new Date(session.getLastAccessedTime());
+
+        System.out.println("BServlet----------SessionId: " + sessionId + " IsNew: " + sessionNayaHaiKya + "  MaxInactiveInterval: " + maxInactiveInterval + "  SessionCreationTime: " + sessionCreationTime + "  SessionLastAccessedTime: " + sessionLastAccessedTime );
+
+        request.getRequestDispatcher("info.html").forward(request, response);
+
+
+    }
+}
